3fa6501c02532f15903276c6925b1d60db72b884
Akmaral <akmaralmuldaseva1@gmail.com>
2025-02-16 17:10:32 +0500

    The final result

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppRunner appRunner = context.getBean(AppRunner.class);
+        appRunner.run();
+    }
+}
